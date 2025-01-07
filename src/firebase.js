0c07b142ddd94867bbde36969a0f18fc9708affb
rojith <rojithr12@gmail.com>
2025-01-07 17:53:21 +0530

    firbase security

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3WAn-6sKhN583addK0uRxYr3PQfpXkBU",
-  authDomain: "checklist-daec6.firebaseapp.com",
-  projectId: "checklist-daec6",
-  storageBucket: "checklist-daec6.firebasestorage.app",
+  apiKey: "AIzaSyBdgnmgnzdg dK0uRxYr3PQf",
+  authDomain: "chapp.com",
+  projectId: "chec6",
+  storageBucket: "chetorage.app",
   messagingSenderId: "567726534587",
-  appId: "1:567726534587:web:7e504061a870d442771e9e",
-  measurementId: "G-VXRKXRGP8H"
+  appId: "1:56:web71e9e",
+  measurementId: "GH"
 };
 
 const app = initializeApp(firebaseConfig);
